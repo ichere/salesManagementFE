5e8c4963b9c36cce2a0093058a950c22bd1e9a73
unknown <izuagbalachere@gmail.com>
2024-06-25 15:13:53 +0100

    fixed deployment error

--- src/components/products/ProductuctTableRow.tsx
+++ src/components/products/ProductuctTableRow.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import { Box } from '@chakra-ui/react'
 
 export const ProductuctTableRow = () => {
   return (
-    <div>ProductuctTableRow</div>
+    <Box>ProductuctTableRow</Box>
   )
 }
